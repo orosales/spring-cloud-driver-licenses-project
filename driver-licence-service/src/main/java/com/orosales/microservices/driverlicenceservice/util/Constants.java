b3cb0d2ed6735d0e8d84ef2981b2116aba2540c5
Rosales Carrasco <omar.rosalescarrasco@unisys.com>
2024-02-19 18:36:47 -0400

    Add .igignore file

--- driver-licence-service/src/main/java/com/orosales/microservices/driverlicenceservice/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.orosales.microservices.driverlicenceservice.util;
-
-public class Constants {
-    public static final String ACTIVE = "ACTIVE";
-    public static final String INACTIVE = "INACTIVE";
-}
